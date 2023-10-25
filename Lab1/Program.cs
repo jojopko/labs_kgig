e8f225915817fff6b12e1d69a83c91494637baf9
jojopko <gamerat.github@gmail.com>
2023-10-25 09:36:35 +0300

    smal refactor

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -21,15 +21,14 @@ public class Program
 
 public class Game : GameWindow
 {
+    private int numbers_vao;
+    private int numbers_ebo;
+    private int indices_count;
+    Shader shader;
     public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
         : base(gameWindowSettings, nativeWindowSettings)
     {
     }
-
-    Shader shader;
-    private int one_vao;
-    private int one_ebo;
-    private int indices_count;
 
     protected override void OnLoad()
     {
@@ -57,7 +56,7 @@ public class Game : GameWindow
         shader.Use();
 
 
-        GL.BindVertexArray(one_vao);
+        GL.BindVertexArray(numbers_vao);
 
         GL.DrawElements(PrimitiveType.Triangles, indices_count, DrawElementsType.UnsignedInt, 0);
 
@@ -67,7 +66,8 @@ public class Game : GameWindow
 
     private void MakeOne()
     {
-        float[] one_vert = {
+        // Илюстрация происходящего в файле
+        float[] numbers_vert = {
             // x y z
             // ЧИСЛО 1
             // 1 прямая
@@ -78,7 +78,6 @@ public class Game : GameWindow
             // 2 наклонная
             -0.5f, 0.0f, 0.0f,   // m-l 4
             -0.45f, 0.0f, 0.0f,  // m-r 5
-
             // ЧИСЛО 2
             // 1 прямая
             0.0f, 0.5f, 0.0f,   // t-l 6
@@ -102,7 +101,7 @@ public class Game : GameWindow
             0.5f, -0.5f, 0.0f,  // b-r 18
         };
 
-        uint[] one_indices = {
+        uint[] numbers_indices = {
             // ЧИСЛО 1
             0, 1, 2,
             2, 3, 1,
@@ -119,83 +118,28 @@ public class Game : GameWindow
             15, 16, 17,
             17, 18, 16
         };
-        indices_count = one_indices.Length;
 
-        int one_vbo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ArrayBuffer, one_vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, one_vert.Length * sizeof(float), one_vert, BufferUsageHint.StaticDraw);
+        indices_count = numbers_indices.Length;
 
-        shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
-
-        one_vao = GL.GenVertexArray();
-        GL.BindVertexArray(one_vao);
-        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-        GL.EnableVertexAttribArray(0);
-
-        GL.BindBuffer(BufferTarget.ArrayBuffer, one_vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, one_vert.Length * sizeof(float), one_vert, BufferUsageHint.StaticDraw);
-
-        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-        GL.EnableVertexAttribArray(0);
-
-        one_ebo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, one_ebo);
-        GL.BufferData(BufferTarget.ElementArrayBuffer, one_indices.Length * sizeof(uint), one_indices, BufferUsageHint.StaticDraw);
-    }
-
-    private void MakeTwo()
-    {
-        float[] one_vert = {
-            // x y z
-            // 1 прямая
-            0.0f, 0.5f, 0.0f,   // t-l 0
-            0.05f, 0.5f, 0.0f,  // t-r 1
-            0.0f, 0.25f, 0.0f,  // b-l 2 
-            0.05f, 0.25f, 0.0f, // b-r 3
-            // 2 прямая
-            // юзает 0 как         t-l
-            0.5f, 0.5f, 0.0f,  //  t-r 4
-            0.0f, 0.45f, 0.0f, //  b-l 5
-            0.5f, 0.45f, 0.0f, //  b-r 6
-            // 3 наклонная
-            // юзает 6 как         t-r
-            0.45f, 0.45f, 0.0f, // t-l 7
-            0.05f, -0.45f, 0.0f,// b-r 8
-            0.0f, -0.45f, 0.0f, // b-l 9
-            // 4 прямая
-            // юзает 9 как         t-l
-            0.5f, -0.45f, 0.0f, // t-r 
-            0.0f, -0.5f, 0.0f,  // b-l
-            0.5f, -0.5f, 0.0f,  // b-r
-
-        };
-
-        uint[] one_indices = {
-            0, 1, 2,
-            2, 3, 1,
-            2, 3, 5,
-            4, 5, 3
-        };
-
-        int one_vbo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ArrayBuffer, one_vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, one_vert.Length * sizeof(float), one_vert, BufferUsageHint.StaticDraw);
+        int numbers_vbo = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, numbers_vbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, numbers_vert.Length * sizeof(float), numbers_vert, BufferUsageHint.StaticDraw);
 
         shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
 
-        one_vao = GL.GenVertexArray();
-        GL.BindVertexArray(one_vao);
+        numbers_vao = GL.GenVertexArray();
+        GL.BindVertexArray(numbers_vao);
         GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
         GL.EnableVertexAttribArray(0);
 
-        GL.BindBuffer(BufferTarget.ArrayBuffer, one_vbo);
-        GL.BufferData(BufferTarget.ArrayBuffer, one_vert.Length * sizeof(float), one_vert, BufferUsageHint.StaticDraw);
+        GL.BindBuffer(BufferTarget.ArrayBuffer, numbers_vbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, numbers_vert.Length * sizeof(float), numbers_vert, BufferUsageHint.StaticDraw);
 
         GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
         GL.EnableVertexAttribArray(0);
 
-        one_ebo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, one_ebo);
-        GL.BufferData(BufferTarget.ElementArrayBuffer, one_indices.Length * sizeof(uint), one_indices, BufferUsageHint.StaticDraw);
+        numbers_ebo = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, numbers_ebo);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, numbers_indices.Length * sizeof(uint), numbers_indices, BufferUsageHint.StaticDraw);
     }
 }
